3302d6dbdee1c368d184c361b973adebdebe1526
andywar65 <andy.war1965@gmail.com>
2021-04-07 23:58:45 +0200

    import pointer lock controls

--- static/buildings/js/photostation_3d.js
+++ static/buildings/js/photostation_3d.js
@@ -1,5 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
+import { PointerLockControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/PointerLockControls.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
