a6b753aa9540e571e81c8b65e80822e0efa3e763
andywar65 <andy.war1965@gmail.com>
2021-04-07 23:13:21 +0200

    script as a module

--- static/buildings/js/photostation_3d.js
+++ static/buildings/js/photostation_3d.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
