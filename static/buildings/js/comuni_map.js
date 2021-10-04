7a6cd3ea06b09add27a7b9076b82ae9fd2e96aff
andywar65 <andy.war1965@gmail.com>
2021-10-04 22:41:37 +0200

    pointing at lazio

--- static/buildings/js/comuni_map.js
+++ static/buildings/js/comuni_map.js
@@ -1,5 +1,8 @@
 const copy = '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const osm = L.tileLayer(url, { attribution: copy })
-const map = L.map('map', { layers: [osm] })
-map.fitWorld();
+const map = L.map('map', {
+  center: [ 41.8988 , 12.5451 ],
+  zoom: 9,
+  layers: [osm] })
+//map.fitWorld();
