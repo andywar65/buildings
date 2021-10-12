9ddc972ce108357d4388af664cb617d3e984619b
andywar65 <andy.war1965@gmail.com>
2021-10-12 22:40:47 +0200

    added map locate

--- static/buildings/js/comuni_map.js
+++ static/buildings/js/comuni_map.js
@@ -1,14 +1,18 @@
+//https://www.paulox.net/2021/07/19/maps-with-django-part-2-geodjango-postgis-and-leaflet/#showing-markers-in-the-map
+
 const copy = '© <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
 const url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const osm = L.tileLayer(url, { attribution: copy })
 const map = L.map('map', { layers: [osm] })
 const layerGroup = L.layerGroup().addTo(map)
-map.setView([41.8988, 12.5451], 9)
+map.locate()
+  .on('locationfound', e => map.setView(e.latlng, 9))
+  .on('locationerror', () => map.setView([41.8988, 12.5451], 9))
 
 async function load_comuni() {
   let comuni_url = ``
   const zoom = map.getZoom()
-  if (zoom<10){
+  if (zoom<14){
     comuni_url = `/edifici/comuni/api/lo/?in_bbox=${map.getBounds().toBBoxString()}`
   } else {
     comuni_url = `/edifici/comuni/api/hi/?in_bbox=${map.getBounds().toBBoxString()}`
