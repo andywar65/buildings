4a69899cc54423755d7681d2cfd8e185d82ef240
andywar65 <andy.war1965@gmail.com>
2021-12-28 16:35:56 +0100

    use new template

--- views/building.py
+++ views/building.py
@@ -49,12 +49,11 @@ class AlertMixin:
                 context[ param ] = self.request.GET[ param ]
         return context
 
-class BuildingListView( TemplateView ):#AlertMixin,
-    template_name = 'buildings/building_list_new.html'
+class BuildingListView( TemplateView ):
+    template_name = 'buildings/building_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        #context = self.add_alerts_to_context(context)
         context['map_data'] = {
             'mapbox_token': settings.MAPBOX_TOKEN,
             'city_lat': settings.CITY_LAT,
